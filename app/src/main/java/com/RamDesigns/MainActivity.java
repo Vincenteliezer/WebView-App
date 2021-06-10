94941d6e1accced03d5918689740d686ed1a0834
Ramesheliezer <80811007+Ramesheliezer@users.noreply.github.com>
2021-06-11 00:42:16 +0300

    project

---
+++ app/src/main/java/com/RamDesigns/MainActivity.java
@@ -0,0 +1,37 @@
+package com.RamDesigns;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview1);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://vincenteliezer.github.io/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
